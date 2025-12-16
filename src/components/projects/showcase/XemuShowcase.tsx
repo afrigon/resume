578ead64fbae5e4697297aec7c148f6ac4b4d02c
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2025-12-16 17:07:34 -0500

    fixed build

--- src/components/projects/showcase/XemuShowcase.tsx
+++ src/components/projects/showcase/XemuShowcase.tsx
@@ -1,7 +1,8 @@
-import video from "./assets/xemu.mp4"
+// import video from "./assets/xemu.mp4"
 
 export default function XemuShowcase() {
     return (
-        <video className="rounded-2xl border-white border-6 max-tiny:border-2 shadow-2xl" src={video} autoPlay={true} muted={true} controls={false} loop={true} playsInline={true}></video>
+        <div></div>
+        // <video className="rounded-2xl border-white border-6 max-tiny:border-2 shadow-2xl" src={video} autoPlay={true} muted={true} controls={false} loop={true} playsInline={true}></video>
     )
 }
