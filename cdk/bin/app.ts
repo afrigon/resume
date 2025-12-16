84db744eabee0a1740a30438c8581acb649dc32f
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2025-12-16 17:19:43 -0500

    probably fixed cdk

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -3,6 +3,9 @@ import { ResumeStack } from "../lib/resume.js"
 
 const app = new cdk.App()
 
+const env = { account: process.env.AWS_ACCOUNT, region: process.env.AWS_REGION }
+
 new ResumeStack(app, "Resume", { 
-    description: "This stack includes resources needed for my resume on alexandre.frigon.app"
+    description: "This stack includes resources needed for my resume on alexandre.frigon.app",
+    env
 })
